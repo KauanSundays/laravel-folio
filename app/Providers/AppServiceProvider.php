916a618025b4a1083ab53e2bfbf819d31b786bfb
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-09-17 08:46:06 -0300

    feat: wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Schema::defaultStringLength(191); // Reduzindo o comprimento padrão para 191 caracteres
     }
 }
